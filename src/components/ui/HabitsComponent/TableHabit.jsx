7bcec5ee33864b4a2809ea3ee545aec8a473ed1b
Andrew Kondratiev <agkondratiev@yandex.ru>
2025-07-29 14:47:39 +0300

    Add in TableHabbit Trash and Pencil

---
+++ src/components/ui/HabitsComponent/TableHabit.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Pencil, Trash2 } from 'lucide-react'
+
+const getDaysInMonth = (year, month) => {
+  const date = new Date(year, month + 1, 0)
+  return Array.from({ length: date.getDate() }, (_, i) => i + 1)
+}
+
+function TableHabit({ habits, year, month }) {
+  const daysInMonth = getDaysInMonth(year, month)
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full border-collapse">
+        <thead>
+          <tr>
+            <th className="border-black border-2 px-2 py-1 bg-gray-100 text-left text-red-600 font-bold">
+              ПРИВЫЧКА
+            </th>
+            <th className="border-black border-2 px-2 py-1 bg-gray-100 text-left text-red-600 font-bold">
+              ДЕЙСТВИЕ
+            </th>
+            {daysInMonth.map((day) => (
+              <th
+                key={day}
+                className="border-black border-2 px-2 py-1 bg-gray-100 text-center text-red-600"
+              >
+                {day}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {habits.map((habit) => (
+            <tr key={habit.id}>
+              <td className="border-white border-2 px-2 py-1 text-red-600">
+                {habit.title}
+              </td>
+              <td className="border-white border-2 px-2 py-1 text-red-600">
+                <button
+                  onClick={() => handleEdit(count.id)}
+                  className="text-blue-500 hover:text-yellow-500 transition ml-2"
+                  title="Редактировать"
+                >
+                  <Pencil size={18} />
+                </button>
+                <button
+                  onClick={() => handleDelete(count.id)}
+                  className="text-blue-500 hover:text-red-500 transition ml-4"
+                  title="Удалить"
+                >
+                  <Trash2 size={18} />
+                </button>
+              </td>
+              {daysInMonth.map((day) => (
+                <td
+                  key={day}
+                  className="border-white border-2 px-2 py-1 text-center"
+                >
+                  <input type="checkbox" />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default TableHabit
--- src/pages/Habits/Habits.jsx
+++ src/pages/Habits/Habits.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 
 function Habits() {
-  return <div className="bg-[whitesmoke] text-blue-500">Habits</div>
+  const [year, setYear] = useState(new Date().getFullYear())
+  const [month, setMonth] = useState(new Date().getMonth())
+  const [habits, setHabits] = useState([]) // список привычек
+
+  const addHabit = (title) => {
+    const newHabit = { id: uuidv4(), title }
+    setHabits((prev) => [...prev, newHabit])
+  }
+
+  return (
+    <div className="w-full mx-auto px-8">
+      <MonthYearControl
+        year={year}
+        setYear={setYear}
+        month={month}
+        setMonth={setMonth}
+      />
+      <AddHabit onAdd={addHabit} />
+      <TableHabit habits={habits} year={year} month={month} />
+    </div>
+  )
 }
 
 export default Habits
